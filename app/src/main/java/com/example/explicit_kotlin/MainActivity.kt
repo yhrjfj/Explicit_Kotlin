54b843fb64920013233787ae402c08c4b98f9f88
Shinigami <102893295+shadowlight671@users.noreply.github.com>
2023-10-14 18:53:52 +0600

    Explicit Intents from 1 activity to another

--- app/src/main/java/com/example/explicit_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/explicit_kotlin/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.explicit_kotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var button : Button = findViewById(R.id.btn)
+
+        button.setOnClickListener(){
+            // Explicit Intents
+            var i = Intent(this, Activity2::class.java)
+            startActivity(i)
+        }
     }
 }
